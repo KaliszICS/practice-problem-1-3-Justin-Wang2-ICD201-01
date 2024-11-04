62e8641baa22dbcab1cea8e52af039422e16e88b
Justin-Wang2-ICD201-01 <justin.wang@student.tdsb.on.ca>
2024-11-04 12:55:33 -0500

    Update PP1_3.py

--- PP1_3.py
+++ PP1_3.py
@@ -1,16 +1,14 @@
-
-
 def q1():
   print (" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n")
   print ("* * * * *\n*       *\n*       *\n*       *\n* * * * *\n")
   print ("* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *\n")
 
-
 def q2():
   print("Hello ", end = "") 
   print("World")
+
 def q3():
-print ('"Man"\n"Board"\n"Man Overboard"')
+  print ('"Man"\n"Board"\n"Man Overboard"')
 
 
 #Do not alter anything after this line
